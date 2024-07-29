c903ee02835fb7f8c0066570289a4f9ea7ff9274
Ashish <ashish.p@lecturenotes.in>
2024-07-29 23:01:54 +0530

    Adding aws- sam and java folder

--- JAVA/String.java
+++ JAVA/String.java
@@ -1,3 +1,4 @@
 int digit = Character.getNumericValue(str.charAt(0));
 int number = Integer.parseInt("12345");
 int[] intArray = str.toCharArray();
+Map<Character, Integer> charCount = new hashMap<>(); -----> charCount.put(c, charCount.getOrDefault(c,0)+1)
