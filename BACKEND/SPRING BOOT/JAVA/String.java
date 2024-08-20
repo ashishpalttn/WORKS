2dfa3d12841c76a65e894ea5fe96ca9ac93913a7
Ashish <ashish.p@lecturenotes.in>
2024-08-20 19:31:30 +0530

    21 Aug from my laptop

--- BACKEND/SPRING BOOT/JAVA/String.java
+++ BACKEND/SPRING BOOT/JAVA/String.java
@@ -1,5 +1,6 @@
 int digit = Character.getNumericValue(str.charAt(0));
 int number = Integer.parseInt("12345");
+Character.isDisit()
 int[] intArray = str.toCharArray();
 Map<Character, Integer> charCount = new hashMap<>(); -----> charCount.put(c, charCount.getOrDefault(c,0)+1)
 Character.isUppperCase(c) -----> check if char is upper case.
