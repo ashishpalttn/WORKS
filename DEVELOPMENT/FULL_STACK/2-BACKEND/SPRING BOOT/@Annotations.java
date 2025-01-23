58666282e1dbfe7b5641a3c087218ed8fc8866af
Ashish Pal <apal@vrhythms.com>
2025-01-23 18:26:11 +0530

    started Spring exploration

---
+++ DEVELOPMENT/FULL_STACK/2-BACKEND/SPRING BOOT/@Annotations.java
@@ -0,0 +1,7 @@
+1- @SpringBootApplication = This is main entry point for a Spring Boot Application.
+
+2- @RestController = Used to define a RESTful controller
+
+3 @RequestMapping = Used to map HTTP request to handle rest controllers. It can be specialized with @GetMapping, @PostMapping, @PutMapping, and @DeleteMapping for specific HTTP methods.
+
+4@Autowired = this annotation used for dependency injection
---
+++ DEVELOPMENT/FULL_STACK/2-BACKEND/SPRING BOOT/term_from_project_exploration.java
@@ -0,0 +1,6 @@
+Serialaizable =  it is a maker interface  that indicates that a class can be serialized, which means  its objects can be convert into byte stream. 
+                           
+
+2 - type of interface ?
+3 - @Slf4j ?
+4 - @Beans ?
