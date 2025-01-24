0b6f7a4c98531fb3469c5cd2b37457fb77184899
Ashish Pal <apal@vrhythms.com>
2025-01-24 10:13:08 +0530

    updating shell script

--- DEVELOPMENT/FULL_STACK/vs code settings/start-all.sh
+++ DEVELOPMENT/FULL_STACK/vs code settings/start-all.sh
@@ -6,6 +6,16 @@ gnome-terminal --tab -- bash -c "cd terminal777888/REACT_FRONTEND_MICROSERVICES/
 gnome-terminal --tab -- bash -c "cd terminal777888/NODE_BACKEND_MICROSERVICES/login-system && npm start; exec bash"
 gnome-terminal --tab -- bash -c "cd terminal777888/NODE_BACKEND_MICROSERVICES/institute-backend && npm start; exec bash"
 
+gnome-terminal \
+--tab --title="UI Tab 1" -- bash -c "cd /Users/ashish/careEdge/care-apps/ui && npm start; exec bash" \
+--tab --title="UI Tab 2" -- bash -c "cd /Users/ashish/careEdge/care-apps/ui && npm start; exec bash" \
+--tab --title="UI Tab 3" -- bash -c "cd /Users/ashish/careEdge/care-apps/ui && npm start; exec bash" \
+--tab --title="UI Tab 4" -- bash -c "cd /Users/ashish/careEdge/care-apps/ui && npm start; exec bash" \
+--tab --title="UI Tab 5" -- bash -c "cd /Users/ashish/careEdge/care-apps/ui && npm start; exec bash"
+
+# make exutable file first
+#          chmod +x start-all.sh
+
 # run the project by file name 
 #            ./start-all.sh
 
