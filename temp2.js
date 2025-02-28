4c9ca2e36ce9708eb56a2782f9467c42a6238166
Ashish <terminal777888@gmail.com>
2025-03-01 00:12:28 +0530

    adding new questions

--- temp2.js
+++ temp2.js
@@ -1,16 +1,14 @@
-const p1 = new Promise((res, rej)=> setTimeout(()=>res('one'),500))
-const p2 = new Promise((res, rej)=>setTimeout(()=>res('two'),1000))
-const p3 = new Promise((res, rej)=>res('three'))
-// const p4 = new Promise((res, rej)=>rej('rejected'))
-
-Promise.all([p1,p2]).then((res)=>console.log(res))
-                    .catch((e)=>console.log(e))
 
 
-const fetchData = () =>{
-    try{
-        Promise.all[
-            fetch('').then(res=>res.json)
-        ]
-    }
-}
+
+
+// const printUserName = (userName) => {
+
+// useEffect (()=>{
+// console.log(`User Name ${userName}`);
+// },[userName])
+
+// }
+
+// printUserName("Ashish")
+
