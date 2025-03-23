ee50418797feaa363b821d2e86999690d961264c
Ashish <terminal777888@gmail.com>
2025-03-24 01:31:05 +0530

    UPDATING note

---
+++ Z-NOTE1.js
@@ -0,0 +1,7 @@
+for(let i=0;i<=9;i++){   // if i do let
+          
+    setTimeout(()=>{
+        console.log(i)  
+    },1000)
+  }
+  //console.log(i)
--- temp2.js
+++
@@ -1,8 +0,0 @@
-const fork = require('fork');
-
-const child = fork('./child')
-
-child.send({"msg":"message from parent"})
-child.on('msg',()=>{
-    
-})
