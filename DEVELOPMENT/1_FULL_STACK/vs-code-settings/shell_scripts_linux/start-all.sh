f63e8e33ee56e71b911c0751f3aad5ec7f4b3f80
Ashish Pal <apal@vrhythms.com>
2025-04-10 18:39:26 +0530

    updating shaell conmd for mack for react ui

---
+++ DEVELOPMENT/1_FULL_STACK/vs-code-settings/shell_scripts_linux/start-all.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Define the paths to your projects
+declare -a paths=(
+    "/Users/ashish/careEdge/portal/ui"
+    "/Users/ashish/careEdge/admin/ui"
+    "/Users/ashish/careEdge/authproxy/ui"
+    "/Users/ashish/careEdge/systemadmin/ui"
+    "/Users/ashish/careEdge/careedge-hos/ui"
+    "/Users/ashish/careEdge/care-apps/ui"
+)
+
+# Iterate over each path and open a new terminal tab to run npm start
+for path in "${paths[@]}"
+do
+    osascript -e "tell application \"Terminal\"
+        activate
+        do script \"cd $path && npm start\"
+    end tell"
+done
+
+# make exutable file first
+#          chmod +x start-all.sh
+
+# run the project by file name 
+#            ./start-all.sh
+
+# kill all process
+#           pkill gnome-terminal
+
