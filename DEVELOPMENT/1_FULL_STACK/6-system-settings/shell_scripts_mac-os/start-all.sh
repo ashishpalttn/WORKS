b407babd3d02caab1965625797244db086c14811
Ashish Pal <apal@vrhythms.com>
2025-04-24 12:19:57 +0530

    updating run scripts

--- DEVELOPMENT/1_FULL_STACK/6-system-settings/shell_scripts_mac-os/start-all.sh
+++ DEVELOPMENT/1_FULL_STACK/6-system-settings/shell_scripts_mac-os/start-all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Define the paths to your projects
+#!/bin/bash
+
+# Define the paths to your projects
 declare -a paths=(
     "/Users/ashish/careEdge/portal/ui"
     "/Users/ashish/careEdge/admin/ui"
@@ -8,6 +11,7 @@ declare -a paths=(
     "/Users/ashish/careEdge/systemadmin/ui"
     "/Users/ashish/careEdge/careedge-hos/ui"
     "/Users/ashish/careEdge/care-apps/ui"
+    "/Users/ashish/careEdge/care-progression/ui"
 )
 
 # Iterate over each path and open a new terminal tab to run npm start
@@ -15,9 +19,10 @@ for path in "${paths[@]}"
 do
     osascript -e "tell application \"Terminal\"
         activate
-        do script \"cd $path && npm start\"
+        do script \"cd $path && npm install &&  npm start\"
     end tell"
 done
+
 
 # make exutable file first
 #          chmod +x start-all.sh
